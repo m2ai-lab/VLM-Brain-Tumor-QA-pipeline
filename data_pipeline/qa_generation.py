4e3bac5b8285c2980fa9ed49ac45239b4635f917
Junayd Lateef <junayd.lateef@ucsf.edu>
2026-03-19 15:06:57 -0700

    added keywords

--- data_pipeline/qa_generation.py
+++ data_pipeline/qa_generation.py
@@ -167,8 +167,10 @@ for idx, row in clinical_notes.iterrows():
 
         #exclude QA pairs with any of these words
         for keyword in ["postsurgical change", "Postsurgical change", "postsurgical changes", "Postsurgical changes", "postsurgical", "Postsurgical", "retrospect", "Retrospect", "progression", 
-                        "Progression", "recurrent", "Recurrent", "stable", "Stable", "tumor growth", "Tumor growth", "tumor shrinkage", "retrospective", "Retrospective",
-                        "Tumor shrinkage", "metastasis", "Metastasis", "spine", "Spine", "spinal", "Spinal", "\\*\\*\\*\\*\\*", "\\*\\*\\*\\*\\*'s", "metastases", "Metastases"]:
+"Progression", "recurrent", "Recurrent", "stable", "Stable", "tumor growth", "Tumor growth", "tumor shrinkage", "retrospective", "Retrospective",
+"Tumor shrinkage", "metastasis", "Metastasis", "spine", "Spine", "spinal", "Spinal", "\\*\\*\\*\\*\\*", "\\*\\*\\*\\*\\*'s", "metastases", "Metastases",
+"diffus\\*", "perfus\\*", "flow", "MRA", "MRV", "metasta\\*", "contrast", "age", "date", "fiducial", "discussed", "specified", "purpose", "radiation", "susceptibility",
+"Diffus\\*", "Perfus\\*", "Flow", "Metasta\\*", "Contrast", "Age", "Date", "Fiducial", "Discussed", "Specified", "Purpose", "Radiation", "Susceptibility"]:
             
             if re.search(rf"{keyword}", entry['question']) != None or re.search(rf"{keyword}", entry['answer']) != None:
                 skip = True #Check if the keyword is found
