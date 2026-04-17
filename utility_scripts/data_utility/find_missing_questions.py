b9cde691c7ee50646baa75a92f18b0b97e351b8a
Shiv Ghosh <46800850+shivghosh@users.noreply.github.com>
2026-04-17 15:07:44 -0700

    Updating the missing question script

--- utility_scripts/data_utility/find_missing_questions.py
+++ utility_scripts/data_utility/find_missing_questions.py
@@ -5,8 +5,8 @@ import os
 
 def argument_handler():
     parser = argparse.ArgumentParser(description="Creating blank Nifti Pipeline")
-    parser.add_argument('--response_path', type=str, required=False, default="/scratch/group/CX000019_DS1/vlm-brain-mri/QApairs/finalized_ucsf_pdgm_pairs.csv")
-    parser.add_argument('--ground_truth_path', type=str, required=False, default="/scratch/group/CX000019_DS1/vlm-brain-mri/QApairs/finalized_ucsf_pdgm_pairs.csv")
+    parser.add_argument('--response_path', type=str, required=False, default="/scratch/group/CX000019_DS1/vlm-brain-mri/finalized_ucsf_pdgm_pairs.csv")
+    parser.add_argument('--ground_truth_path', type=str, required=False, default="/scratch/group/CX000019_DS1/vlm-brain-mri/finalized_ucsf_pdgm_pairs.csv")
     return parser.parse_args()
 
 def main(args):
@@ -18,21 +18,23 @@ def main(args):
     ground_truth_df["predicted_answer"] = ["Invalid Response"] * len(ground_truth_df)
     
     #Get all correct Answers for a question from the response df
+
+    output_rows = []
     for _,row in response_df.iterrows():
         assigned_id = row['question_id']
-        answer = row['Answer']
-        possible_questions = ground_truth_df[["Assigned ID" == assigned_id]]["Question"]
+        answer = row['predicted_answer']
+        possible_questions = ground_truth_df[ground_truth_df["Assigned ID"] == assigned_id]["Question"]
         for possible_question in possible_questions:
             if answer in possible_question:
                 response_df.loc[response_df['question_id'] == assigned_id, 'Question'] = possible_question
                 break
     for _,row in ground_truth_df.iterrows():
         if row["Question"] not in response_df["Question"].values:
-            output_df.append(row)
+            output_rows.append(row)
         else:
-            output_df.append(response_df.loc[response_df['Question'] == row["Question"]])
+            output_rows.append(response_df.loc[response_df['Question'] == row["Question"]])
 
-     
+    output_df = pd.DataFrame(output_rows)
     output_dir = os.path.dirname(args.response_path)
     output_filename = os.path.basename(args.response_path)
     output_filename = output_filename.replace(".csv", "_matched.csv")
