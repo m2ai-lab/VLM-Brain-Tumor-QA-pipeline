086be0ba5b428c00cdde47680448780dc3c43f76
Shiv Ghosh <46800850+shivghosh@users.noreply.github.com>
2026-05-05 13:56:54 -0700

    Moving up another level

--- utility_scripts/data_utility/derive_human_results.py
+++ utility_scripts/data_utility/derive_human_results.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 
-_PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+_PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 if _PROJECT_ROOT not in sys.path:
     sys.path.insert(0, _PROJECT_ROOT)
 from config_utils import load_config
