2062aa0a7ce45924ee493bbe0aa69a2f95b1f05b
Junayd Lateef <junayd.lateef@ucsf.edu>
2026-05-06 19:58:11 -0700

    Update heuristic_data_filter.py
    added remaining pairs

--- data_pipeline/heuristic_data_filter.py
+++ data_pipeline/heuristic_data_filter.py
@@ -16,11 +16,13 @@ for idx, entry in frames.iterrows():
 "Progression", "recurrent", "Recurrent", "stable", "Stable", "tumor growth", "Tumor growth", "tumor shrinkage", "retrospective", "Retrospective",
 "Tumor shrinkage", "metastasis", "Metastasis", "spine", "Spine", "spinal", "Spinal", "\\*\\*\\*\\*\\*", "\\*\\*\\*\\*\\*'s", "metastases", "Metastases",
 "diffus\\*", "perfus\\*", "flow", "MRA", "MRV", "metasta\\*", "contrast", "age", "date", "fiducial", "discussed", "specified", "purpose", "radiation", "susceptibility",
-"Diffus\\*", "Perfus\\*", "Flow", "Metasta\\*", "Contrast", "Age", "Date", "Fiducial", "Discussed", "Specified", "Purpose", "Radiation", "Susceptibility"]:
+"Diffus\\*", "Perfus\\*", "Flow", "Metasta\\*", "Contrast", "Age", "Date", "Fiducial", "Discussed", "Specified", "Purpose", "Radiation", "Susceptibility", 
+"report", "Report", "Necrosis", "necrosis", "Necrotic", "necrotic", "Metastatic", "metastatic", "Diffusion", "diffusion"]:
             
             if re.search(rf"{keyword}", entry['Question']) != None or re.search(rf"{keyword}", str(entry['Answer'])) != None:
                 indecies.append(idx)
 
 frames = frames.drop(indecies)
 print(frames.shape)
-frames.to_csv('path/to/ucsf_neuroimaging_pairs_0_9000.csv')
+
+frames.to_csv('path/to/new_pairs.csv')
